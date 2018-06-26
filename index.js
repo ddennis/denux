f7fadf54c682b04973f03e2b3fc7ef709593a3b1
Dennis Christoffersen <dennis@fantastisk.dk>
2018-06-26 11:12:17 +0200

    refactor: es5 version is now exported by default

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
 /**
- * Created by @author @ddennis - ddennis.dk aka fantastisk.dk/works aka meresukker.dk on 25-06-2018.
+ * Created by @author @ddennis - ddennis.dk aka fantastisk.dk/works aka meresukker.dk on 26-06-2018.
  */
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 const Context = React.createContext();
 
-export default class Denux extends Component {
+export class Denux extends Component {
+	constructor(...args) {
+		var _temp;
 
-	static Consumer = Context.Consumer;
+		return _temp = super(...args), this.state = _extends({}, this.props.state, { dispatch: action => {
+				this.setState(state => this.props.reducer(state, action));
+			}
+		}), _temp;
+	}
 
 	// Static utility function - stolen from redux
-	static combineReducers = (reducers) => {
+	static combineReducers(reducers) {
 		return (state = {}, action) => {
-			return Object.keys(reducers).reduce(
-				(nextState, key) => {
-					nextState[key] = reducers[key](state[key], action );
-					return nextState;
-				},
-				{}
-			);
+			return Object.keys(reducers).reduce((nextState, key) => {
+				nextState[key] = reducers[key](state[key], action);
+				return nextState;
+			}, {});
 		};
-	};
-
-
-	state = {
-		...this.props.state, dispatch:(action) =>{
-			this.setState(
-				state => this.props.reducer(state, action)
-			);
-		}
-	};
-
+	}
 
 	render() {
-		return <Context.Provider value={this.state}>{this.props.children}</Context.Provider>;
+		return React.createElement(
+			Context.Provider,
+			{ value: this.state },
+			this.props.children
+		);
 	}
 
 }
-
+Denux.Consumer = Context.Consumer;
